613e4f3f81c1e4e177f2a0acb3834d7cf1412b30
Chad-Mowbray <mowbray.chad@gmail.com>
2019-11-24 12:17:37 -0600

    initial

---
+++ src/ChangeTheWorld.js
@@ -0,0 +1,19 @@
+
+const fizzBuzz = function(num) {
+    let answer = ''
+    for(let i = 1; i <= num; i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            answer +='fizzBuzz '
+        } else if(i % 3 == 0) {
+            answer += 'fizz '
+        // } else if(i % 5 == 0) {
+        //     answer += 'buzz '
+        } else {
+            answer += i + ' '
+        }
+    }
+    return answer
+}
+
+const num = process.env.NUM
+console.log(fizzBuzz(num))
---
+++ tests/testSuite.js
@@ -0,0 +1,40 @@
+//automated pipeline tests
+
+let answer = process.env.ANSWER
+answer = answer.split(' ')
+
+let testResults = {
+    test1: '',
+    test2: '',
+    test3: ''
+}
+
+const test1 = function(firstFizz) {
+    if(firstFizz !== 'fizz') {
+        testResults.test1 = 'failed'
+    } else {
+        testResults.test1 = 'passed'
+    }
+}
+
+const test2 = function(firstBuzz) {
+    if(firstBuzz !== 'buzz') {
+        testResults.test2 = 'failed'
+    } else {
+        testResults.test2 = 'passed'
+    }
+}
+
+const test3 = function(firstFizzBuzz) {
+    if(firstFizzBuzz !== 'fizzBuzz') {
+        testResults.test3 = 'failed'
+    } else {
+        testResults.test3 = 'passed'
+    }
+}
+
+test1(answer[2])
+test2(answer[4])
+test3(answer[14])
+
+console.log(testResults)
