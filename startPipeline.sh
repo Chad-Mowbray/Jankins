68134c1e62a74f35f134dcb0aaeb22f6e84637f3
Chad Mowbray <chad.mowbray@motorolasolutions.com>
2019-11-25 15:53:18 -0600

    simplify config step

--- startPipeline.sh
+++ startPipeline.sh
@@ -4,34 +4,27 @@ clear
 echo Welcome to Your Jankins CI/CD Pipeline!
 echo ---------------------------------------
 echo
-echo -n "Configure your environment dev/prod: "
-read TESTENV
-echo -Configuring environment...
 
 
-if [[ "$TESTENV" == "dev" ]]; then
-    CONFIGNUM=$(cat ./config/developmentEnv.txt)
-fi
+echo --Configuring environment...
+CONFIGNUM=$(cat ./config/developmentEnv.txt)
 
-if [[ "$TESTENV" == "prod" ]]; then
-    CONFIGNUM=$(cat ./config/productionEnv.txt)
-fi
-
-
-echo -Running code...
+echo
+echo --Running code...
 NUM=$CONFIGNUM node ./src/fizzBuzzFeature.js > answer.txt
 
-
-echo -Running Tests...
+echo
+echo --Running Tests...
 ANSWER=$(cat ./answer.txt)
 ANSWER=$ANSWER node ./tests/testSuite.js > testResults.txt
 
-
-echo -Emailing results...
+echo
+echo --Emailing results...
 cp ./email/emailForm.txt ./email/emailResults.txt
 cat ./testResults.txt >> ./email/emailResults.txt
 #curl -s --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from 'EXAMPLE EMAIL --mail-rcpt 'EXAMPLE EMAIL' --upload-file ./email/emailResults.txt --user 'EMAIL:PASSWORD'
 
-
-echo -Cleaning up...
+echo
+echo --Cleaning up...
 rm ./answer.txt ./testResults.txt ./email/emailResults.txt
+echo
